f25d66c519fcd90067e472a319a6ad4d6a072f2c
Alexey Talkan <petrovi4@gmail.com>
2019-04-12 02:42:48 +0300

    Fast Fix

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,4 +1,8 @@
+mkdir -p $NVM_DIR
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 node_version="lts/dubnium"
 nvm install $node_version
@@ -7,8 +11,9 @@ nvm alias default $node_version
 
 if test ! $(which spoof)
 then
-  if test $(which npm)
-  then
-    sudo npm install spoof -g
-  fi
+	if test "$(uname)" = "Darwin"; then 
+		sudo npm install spoof -g
+	elif test "$(uname)" = "Linux"; then 
+		sudo npm install spoof -g
+	fi
 fi
