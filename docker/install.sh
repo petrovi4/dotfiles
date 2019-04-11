253bf35cb7f445c82fb6b2b4ea05acb7e408a320
Alexey Talkan <petrovi4@gmail.com>
2019-04-12 02:54:19 +0300

    Fast Fix

--- docker/install.sh
+++ docker/install.sh
@@ -1,9 +1,9 @@
-	if test "$(uname)" = "Darwin"; then 
-		brew install docker
-	elif test "$(uname)" = "Linux"; then 
-		apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-		curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-		add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-		apt-get update
-		apt-get install docker-ce docker-ce-cli containerd.io
-	fi
+if test "$(uname)" = "Darwin"; then 
+	brew install docker
+elif test "$(uname)" = "Linux"; then 
+	apt-get --assume-yes install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+	apt-get update
+	apt-get --assume-yes install docker-ce docker-ce-cli containerd.io
+fi
