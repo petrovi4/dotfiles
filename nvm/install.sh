5bf674f054e8110b40f435400146e6bf9c6502a1
Alexey Talkan <petrovi4@gmail.com>
2019-04-12 15:29:48 +0300

    Fast Fix

--- nvm/install.sh
+++ nvm/install.sh
@@ -2,6 +2,7 @@ mkdir -p $NVM_DIR
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 node_version="lts/dubnium"
