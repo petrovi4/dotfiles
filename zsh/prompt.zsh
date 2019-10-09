ad44ee8b39bafb710d4686dba2cb7751e74c6d59
Alexey Talkan <petrovi4@gmail.com>
2019-10-10 01:06:37 +0300

    Почистил промт

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,1 +1,19 @@
-export PROMPT='%{%F{red}%}%n%{%f%}@%{%F{blue}%}%m %{%F{yellow}%}%d %{$%f%}%'
+# Генератор здесь
+# https://zsh-prompt-generator.site/
+
+if (( $+commands[git] ))
+then
+  git="$commands[git]"
+else
+  git="/usr/bin/git"
+fi
+
+git_branch() {
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
+
+
+
+PROMPT="%F{88}%n%f@%F{14}%m%f %F{221}%~%f%F{221}/%f "
+
+RPROMPT=$'%F{88}$(git_branch)%f'
