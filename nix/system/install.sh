4748ebb8e159d105a0790da982cc067d14eb0391
Alexey Talkan <petrovi4@gmail.com>
2019-10-14 02:18:42 +0300

    Добавил установку zsh в линуксе при установке system.sh

--- nix/system/install.sh
+++ nix/system/install.sh
@@ -1,7 +1,7 @@
 
 apt-get update
 
-apt-get --assume-yes install csh build-essential htop
+apt-get --assume-yes install zsh csh build-essential htop
 
 if ! [ -f ~/.ssh/id_rsa ]; then
 	ssh-keygen -f ~/.ssh/id_rsa -t rsa -b 4096 -N ""
