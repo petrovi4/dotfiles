6b00ebe71a8e8dc5b13c3700ff5552cb93fecf4a
petrovi4 <petrovi4@gmail.com>
2021-06-28 23:19:52 +0300

    Fast Fix

--- nix/system/install.sh
+++ nix/system/install.sh
@@ -1,4 +1,3 @@
-
 apt-get update
 
 apt-get --assume-yes install zsh csh build-essential htop
@@ -6,9 +5,8 @@ apt-get --assume-yes install zsh csh build-essential htop
 if ! [ -f ~/.ssh/id_rsa ]; then
 	ssh-keygen -f ~/.ssh/id_rsa -t rsa -b 4096 -N ""
 fi
-	
 
-find $ZSH/nix -name install.sh -not -path "*/system/*" | while read file; do
+find $ZSH -name install.sh -not -path "*/system/*" | while read file; do
 	chmod +x $file
-  $file
+	$file
 done
