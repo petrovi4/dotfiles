970a66267a39557f916e2963cd9f88801b17e115
petrovi4 <petrovi4@gmail.com>
2021-06-29 00:52:23 +0300

    Fast Fix

--- nix/git/aliases.zsh
+++ nix/git/aliases.zsh
@@ -1,4 +1,4 @@
-# Облегчённый набор алиасов для сервера - нет редактирования
+# Light set of aliases for server - no commands for modification
 
 alias gl='git log'
 alias gll="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
