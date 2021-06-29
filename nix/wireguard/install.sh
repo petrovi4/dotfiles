5e8a0b9e926c94a4aac3e070a8a03f6ebf740357
petrovi4 <petrovi4@gmail.com>
2021-06-29 03:21:19 +0300

    Fast Fix

--- nix/wireguard/install.sh
+++ nix/wireguard/install.sh
@@ -2,6 +2,6 @@ echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
 apt-get update
 
-apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//')
+apt-get --assume-yes install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//')
 
 apt-get install wireguard -y
