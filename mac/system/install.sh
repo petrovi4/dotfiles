3b3e8ee065b558300518b0462fb0f4aa49947a72
petrovi4 <petrovi4@gmail.com>
2022-02-18 14:58:22 +0300

    Added some casks, setup script for launchAgents

--- mac/system/install.sh
+++ mac/system/install.sh
@@ -7,11 +7,30 @@ echo "› Updating MacOs ..."
 sudo softwareupdate -i -a
 
 # shortcut to this dotfiles path is $SYSTEM_ROOT
-SYSTEM_ROOT=$HOME/.dotfiles/nix
+SYSTEM_ROOT=$HOME/.dotfiles/mac
 
 $SYSTEM_ROOT/homebrew/install.sh
 
+echo "› Setting up all install files"
 find $SYSTEM_ROOT -name install.sh -not -path "*/homebrew/*" -not -path "*/system/*" | while read file; do
 	chmod +x $file
 	. $file
+done
+
+echo "› Set up launch agents"
+mkdir -p ~/Library/LaunchAgents
+for agent in $(ls $SYSTEM_ROOT/launchAgents); do
+	source_file="$SYSTEM_ROOT/launchAgents/$(basename $agent)"
+	target_file="$HOME/Library/LaunchAgents/$(basename $agent)"
+
+	if [[ -f "$target_file" ]]; then
+		echo "#=> Unload $target_file"
+		launchctl unload "$target_file"
+	fi
+
+	echo "#=> Copy $agent to ~/Library/LaunchAgents"
+	cp "$source_file" "$target_file"
+
+	echo "#=> Load $target_file"
+	launchctl load "$target_file"
 done
