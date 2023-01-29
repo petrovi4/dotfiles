a622ffd3d63c8323c7b77eb713e61679baeea972
petrovi4 <petrovi4@gmail.com>
2023-01-29 19:10:07 +0200

    Добавил aws в brew, изменил пейджирование git и что-то еще

--- mac/zsh/config.zsh
+++ mac/zsh/config.zsh
@@ -14,16 +14,19 @@ setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS   # allow functions to have local traps
+
 setopt HIST_VERIFY
-# setopt SHARE_HISTORY    # share history between sessions ???
+setopt no_share_history
+unsetopt share_history
+# setopt APPEND_HISTORY                   # adds history
+# setopt INC_APPEND_HISTORY SHARE_HISTORY # adds history incrementally and share it across sessions
+
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 #setopt CORRECT
 #setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
-setopt APPEND_HISTORY                   # adds history
-# setopt INC_APPEND_HISTORY SHARE_HISTORY # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS             # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
